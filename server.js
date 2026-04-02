6330f847a0c5ba1c5c652a709104b375b786a80b
Sergio Partida <sheko3857@gmail.com>
2026-04-02 03:11:33 +0000

    fix: resolve merge conflict in server.js

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ const PORT = process.env.PORT || 3000;
 const startServer = async () => {
   try {
     await sequelize.authenticate();
+<<<<<<< Updated upstream
+=======
+    await sequelize.sync({alter:false});
+>>>>>>> Stashed changes
 
-    app.listen(PORT, () => {
-      console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
-    });
+    app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor ejecutandose en puerto ${PORT}`);
+     });
+
+   
+   
   } catch (error) {
     console.error('Error al iniciar el servidor:', error.message);
-    process.exit(1);
+   
+
   }
 };
 
